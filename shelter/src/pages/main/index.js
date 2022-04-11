d27c893e6b050db070960b8a4f733cf8a1d93384
Jaslan <Jaslan.dlnv@gmail.com>
2022-04-12 04:04:45 +0500

    feat: add interactive on elements

--- shelter/src/pages/main/index.js
+++ shelter/src/pages/main/index.js
@@ -1,8 +1,6 @@
 import './index.scss';
-import './css/header.scss';
 import './css/start-screen.scss';
 import './css/about.scss';
 import './css/pets.scss';
 import './css/help.scss';
 import './css/donation.scss';
-import './css/footer.scss';
