ba8aee17a0a1ab891867411c0ea73cfd485f4c1e
Jaslan <Jaslan.dlnv@gmail.com>
2022-04-11 22:34:44 +0500

    feat: add our pets page

--- shelter/src/index.js
+++ shelter/src/index.js
@@ -1,3 +1,6 @@
 import 'normalize.css';
 import './index.scss';
 import './pages/main/index';
+import pets from './pages/pets/index';
+
+pets();
---
+++ shelter/src/pages/pets/index.js
@@ -0,0 +1,2 @@
+import './index.scss';
+import './css/our-pets.scss';
