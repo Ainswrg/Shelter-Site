bcab1e44294c18e8d6fe4c420c1defe40d98dc30
Jaslan <Jaslan.dlnv@gmail.com>
2022-04-12 18:06:02 +0500

    refactor: paginate active disable

--- shelter/webpack.config.js
+++ shelter/webpack.config.js
@@ -30,6 +30,9 @@ const baseConfig = {
       {
         test: /\.(woff(2)?|eot|ttf|otf|)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'assets/fonts/[hash][ext]',
+        },
       },
     ],
   },
