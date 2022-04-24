9119b6938e9f7a7675cce6b91cf790f334fd4b49
Jaslan <Jaslan.dlnv@gmail.com>
2022-04-24 20:11:08 +0500

    feat: implement burger and minor refactor

--- shelter/src/index.js
+++ shelter/src/index.js
@@ -2,5 +2,8 @@ import 'normalize.css';
 import './index.scss';
 import './pages/main/index';
 import pets from './pages/pets/index';
+import { menu, onOverlayClick } from './pages/main/index';
 
 pets();
+menu();
+onOverlayClick();
