41886f9041ae478da314d82e4bd5d7a24e1d3514
Jaslan <Jaslan.dlnv@gmail.com>
2022-04-27 00:05:30 +0500

    feat: implement random generate

--- shelter/src/index.js
+++ shelter/src/index.js
@@ -4,37 +4,42 @@ import './index.scss';
 import './pages/main/index';
 import './js';
 import { SwiperMain, SwiperPets } from './js';
-import getData from './js/carouselData';
+import getData from './js/data';
+import { random } from './js/random';
 // import { Modal } from './js/Modal';
 import { Card } from './js/Card';
 import { CardModule } from './js/CardModule';
 
 const dataCards = getData();
-const dataForMain1 = [];
-const mixRandom = () => {
-  return Math.random() - 0.5;
-};
-for (let i = 0; i < 6; i++) {
-  dataForMain1.push(...dataCards.sort(mixRandom));
+let dataArr = '';
+const mediaQuery = window.matchMedia('(min-width: 1280px)');
+const mediaQuery1 = window.matchMedia('(max-width: 1279px)');
+const mediaQuery2 = window.matchMedia('(max-width: 767px)');
+
+if (mediaQuery.matches) {
+  dataArr = random(dataCards, 6, 0);
+}
+if (mediaQuery1.matches) {
+  dataArr = random(dataCards, 8, 2);
+}
+if (mediaQuery2.matches) {
+  dataArr = random(dataCards, 16, 5);
 }
 
 const mainPage = document.querySelector('#main-page');
 const petsPage = document.querySelector('#pets-page');
 
 window.onload = () => {
-  console.log('Complete');
   //render card
-  if (dataForMain1) {
+
+  if (dataArr) {
     renderDataToDom();
-    // console.log(dataForMain1);
   }
   if (mainPage) {
-    console.log(mainPage);
     SwiperMain();
     addCardClickerHandler();
   }
   if (petsPage) {
-    console.log(petsPage);
     SwiperPets();
     addCardClickerHandler();
   }
@@ -45,15 +50,13 @@ window.onload = () => {
 
 function renderDataToDom() {
   const cardsWrapper = getCardsWrapper();
-  generateCards(dataForMain1).forEach((card) => {
+  generateCards(dataArr).forEach((card) => {
     cardsWrapper.append(card.generateCard());
-    console.log('generate');
   });
 }
 
 function getCardsWrapper() {
   const cardsContainer = document.querySelector('.cards-wrapper');
-  console.log(cardsContainer, 'asda');
   cardsContainer.innerHTML = '';
   return cardsContainer;
 }
@@ -66,26 +69,8 @@ function generateCards(data) {
   return cards;
 }
 
-// function addToolsClickHandler() {
-//   document.querySelectorAll('.card').forEach((card) => {
-//     card.addEventListener('click', () => {
-//       generateToolsModal();
-//     });
-//   });
-// }
-
-// function generateToolsModal() {
-//   renderModalWindow('content');
-// }
-
-// function renderModalWindow(content) {
-//   const modal = new Modal('tools-modal');
-//   modal.buildModal(content);
-// }
-
 function addCardClickerHandler() {
   document.querySelector('.cards-wrapper').addEventListener('click', (e) => {
-    console.log(e.target.className);
     if (e.target.closest('.card')) {
       const clickedCardName = e.target.closest('.card').getAttribute('data-id');
       const clickedCardData = getClickedData(clickedCardName);
@@ -96,7 +81,7 @@ function addCardClickerHandler() {
 }
 
 function getClickedData(name) {
-  return dataForMain1.find((card) => card.name == name);
+  return dataArr.find((card) => card.name == name);
 }
 
 function renderCardModalWindow(card) {
